cb1b3d4630392db589f868a7682933c3f7789e05
Diabel <diabel@diabel.net>
2025-03-31 01:13:41 +0200

    feature[recipes]: started

---
+++ app/Models/Recipe.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Recipe extends Model
+{
+    /** @use HasFactory<\Database\Factories\RecipeFactory> */
+    use HasFactory;
+
+    public function creator(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'creator_id');
+    }
+}
