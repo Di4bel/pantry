be681403ac852509b682f7aa2f97ef4f9132b191
Diabel <diabel@diabel.net>
2025-07-03 00:52:47 +0200

    feat(recipe): finished the recipe creating view and validation, changed index view for none recipes found

--- resources/views/livewire/recipes/recipe-create.blade.php
+++ resources/views/livewire/recipes/recipe-create.blade.php
@@ -1,15 +1,12 @@
 <?php
 
-use function Livewire\Volt\{state,mount};
+use function Livewire\Volt\{rules, state, mount};
 
 
 state([
-    'recipe' => [
-        'title' => '',
-        'creator_id' => null,
-        'ingredients' => [],
-        'description' => '',
-    ],
+    'title' => '',
+    'ingredients' => [],
+    'description' => '',
     'newIngredientName',
     'newIngredientAmount',
     'newIngredientType' => '',
@@ -21,14 +18,31 @@ state([
         'g',
         'tbsp',
         'tsp',
-    ]
+    ],
 ]);
 
-mount(function () {
-    $this->recipe['creator_id'] = Auth::user()->id;
-});
+rules([
+    'title' => 'string|required',
+    'ingredients' => 'array|required',
+    'ingredients.*.name' => 'string|required|max:255',
+    'ingredients.*.amount' => 'numeric|required',
+    'ingredients.*.type' => [
+        'string',
+        'required',
+        \Illuminate\Validation\Rule::in([
+            'count',
+            'kg',
+            'ml',
+            'l',
+            'g',
+            'tbsp',
+            'tsp',
+        ]),
+    ],
+    'description' => 'string|required|regex:/^.+$/'
+]);
 
-$addIngredient = function (){
+$addIngredient = function () {
     $validated = $this->validate([
         'newIngredientName' => 'string|required|max:255',
         'newIngredientAmount' => 'numeric|required',
@@ -39,25 +53,35 @@ $addIngredient = function (){
         ]
     ]);
 
-    $this->recipe['ingredients'][] = [
+    $this->ingredients[] = [
         'name' => $validated['newIngredientName'],
         'amount' => $validated['newIngredientAmount'],
         'type' => $validated['newIngredientType']
     ];
-    $this->reset('newIngredientName','newIngredientAmount');
+    $this->reset('newIngredientName', 'newIngredientAmount');
     $this->newIngredientType = '';
 };
 
-$removeIngredient =  function ($key) {
+$removeIngredient = function ($key) {
     unset($this->recipe['ingredients'][$key]);
 };
+
+$saveRecipe = function () {
+    $action = new \App\Actions\CreateRecipeAction();
+    $validated = $this->validate();
+    $recipe = $action->handle(auth()->user(),$validated);
+    to_route('recipes.show', $recipe);
+}
 ?>
 <div class="h-1/4 flex flex-col">
+    <div class="p-2 text-right">
+        <flux:button variant="primary" icon:trailing="plus" wire:click="saveRecipe()" >Save Recipe</flux:button>
+    </div>
     <div class="p-2">
         <flux:field>
             <flux:label>Title:</flux:label>
-            <flux:input type="text" wire:model.live="recipe.title" />
-            <flux:error name="recipe.title" />
+            <flux:input type="text" wire:model.live="title"/>
+            <flux:error name="title"/>
         </flux:field>
     </div>
     <div class="p-2">
@@ -65,59 +89,65 @@ $removeIngredient =  function ($key) {
             <flux:legend>Ingredients</flux:legend>
             <flux:input.group>
                 <flux:field class="w-full">
-                    <flux:input wire:model="newIngredientName" type="text" placeholder="Ingredient" />
-                    <flux:error name="newIngredientName" />
+                    <flux:input wire:model="newIngredientName" type="text" placeholder="Ingredient"/>
+                    <flux:error name="newIngredientName"/>
                 </flux:field>
                 <flux:field class="w-full">
-                    <flux:input wire:model="newIngredientAmount" type="number" placeholder="Quantity" />
-                    <flux:error name="newIngredientAmount" />
+                    <flux:input wire:model="newIngredientAmount" type="number" placeholder="Quantity"/>
+                    <flux:error name="newIngredientAmount"/>
                 </flux:field>
                 <flux:field class="w-full">
                     <flux:select wire:model="newIngredientType" placeholder="Type...">
-                        <flux:select.option >count</flux:select.option>
-                        <flux:select.option >kg</flux:select.option>
-                        <flux:select.option >ml</flux:select.option>
-                        <flux:select.option >l</flux:select.option>
-                        <flux:select.option >g</flux:select.option>
-                        <flux:select.option >tbsp</flux:select.option>
-                        <flux:select.option >tsp</flux:select.option>
+                        <flux:select.option>count</flux:select.option>
+                        <flux:select.option>kg</flux:select.option>
+                        <flux:select.option>ml</flux:select.option>
+                        <flux:select.option>l</flux:select.option>
+                        <flux:select.option>g</flux:select.option>
+                        <flux:select.option>tbsp</flux:select.option>
+                        <flux:select.option>tsp</flux:select.option>
                     </flux:select>
-                    <flux:error name="newIngredientType" />
+                    <flux:error name="newIngredientType"/>
                 </flux:field>
-                 <flux:button.group>
+                <flux:button.group>
                     <flux:button wire:click="addIngredient" icon="plus"/>
-                 </flux:button.group>
+                </flux:button.group>
             </flux:input.group>
-            <div class="">
-                @foreach($recipe['ingredients'] as $key => $ingredient)
+            <flux:error name="ingredients"/>
+            <div class="mt-2">
+                @foreach($ingredients as $key => $ingredient)
 
-                        <flux:input.group>
-                            <flux:input wire:model="recipe.ingredients.{{$key}}.name" type="text" placeholder="Ingredient" />
-                            <flux:input wire:model="recipe.ingredients.{{$key}}.amount" type="number" placeholder="Quantity" />
-                            <flux:select wire:model="recipe.ingredients.{{$key}}.type" placeholder="Type">
-                                <flux:select.option value="count">count</flux:select.option>
-                                <flux:select.option value="kg">kg</flux:select.option>
-                                <flux:select.option value="ml">ml</flux:select.option>
-                                <flux:select.option value="l">l</flux:select.option>
-                                <flux:select.option value="g">g</flux:select.option>
-                                <flux:select.option value="tbsp">tbsp</flux:select.option>
-                                <flux:select.option value="tsp">tsp</flux:select.option>
-                            </flux:select>
-                            <flux:button.group>
-                                <flux:button  wire:click="duplicateIngredient({{$key}})" icon="clipboard-document"  />
-                                <flux:button variant="danger" wire:click="removeIngredient({{$key}})" icon="minus"  />
-                            </flux:button.group>
-                        </flux:input.group>
+                    <flux:input.group>
+                        <flux:input wire:model="ingredients.{{$key}}.name" type="text" placeholder="Ingredient"/>
+                        <flux:input wire:model="ingredients.{{$key}}.amount" type="number"
+                                    placeholder="Quantity"/>
+                        <flux:select wire:model="ingredients.{{$key}}.type" placeholder="Type">
+                            <flux:select.option value="count">count</flux:select.option>
+                            <flux:select.option value="kg">kg</flux:select.option>
+                            <flux:select.option value="ml">ml</flux:select.option>
+                            <flux:select.option value="l">l</flux:select.option>
+                            <flux:select.option value="g">g</flux:select.option>
+                            <flux:select.option value="tbsp">tbsp</flux:select.option>
+                            <flux:select.option value="tsp">tsp</flux:select.option>
+                        </flux:select>
+                        <flux:button.group>
+                            <flux:button wire:click="duplicateIngredient({{$key}})" icon="clipboard-document"/>
+                            <flux:button variant="danger" wire:click="removeIngredient({{$key}})" icon="minus"/>
+                        </flux:button.group>
+                    </flux:input.group>
 
                 @endforeach
             </div>
         </flux:fieldset>
     </div>
-    <div class="flex-grow p-2">
-        <flux:field>
-            <flux:label>Description:</flux:label>
-            <x-editor wire:model="recipe.description" class="dark:bg-gray-800 dark:text-white rounded h-full" ></x-editor>
-            <flux:error name="recipe.description" />
-        </flux:field>
+    <div class="flex-grow p-2 ">
+        <div>
+            <flux:field>
+                <flux:label>Description:</flux:label>
+                <x-editor wire:model="description"
+                          class="dark:bg-gray-800 dark:text-white rounded h-full"></x-editor>
+
+            </flux:field>
+        </div>
+        <flux:error class="mt-16" name="description"/>
     </div>
 </div>
--- resources/views/livewire/recipes/recipe-index.blade.php
+++ resources/views/livewire/recipes/recipe-index.blade.php
@@ -30,22 +30,29 @@ class extends Component {
         <flux:button variant="primary" href="{{route('recipes.create')}}" >{{ __('Create Recipe') }}</flux:button>
     </div>
     <flux:separator class="my-4"/>
-    <div class="p-2">
-        <table id="recipes" class="border-gray-200 rounded overflow-hidden w-full">
-            <thead>
+
+    @if(!blank($recipes))
+        <div class="p-2">
+            <table id="recipes" class="border-gray-200 rounded overflow-hidden w-full">
+                <thead>
                 <tr class="">
                     <th class="p-2 text-left border max-w-xs truncate">Title</th>
                     <th class="p-2 text-left border">Creator</th>
                 </tr>
-            </thead>
-            <tbody>
+                </thead>
+                <tbody>
                 @foreach($recipes as $recipe)
                     <tr class="border border-gray-200">
                         <td class="p-2 border"><a class="hover:text-pink-300" href="{{route('recipes.show',$recipe)}}">{{$recipe->title}}</a> </td>
                         <td class="p-2 border"><flux:avatar as="button" href="{{route('users.show',$recipe->creator)}}" name="{{$recipe->creator->name}}" color="auto" color:seed="{{$recipe->creator->id}}" /></td>
                     </tr>
                 @endforeach
-            </tbody>
-        </table>
-    </div>
+                </tbody>
+            </table>
+        </div>
+    @else
+        <flux:heading size="xl" level="3" class="text-center">0 Recipes found!</flux:heading>
+        <flux:spacer class="m-2" />
+        <flux:heading size="xl" level="3" class="text-center"><flux:button  href="{{route('recipes.create')}}">Create some awesome new Recipes!</flux:button></flux:heading>
+    @endif
 </div>
